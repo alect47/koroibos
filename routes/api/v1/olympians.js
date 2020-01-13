705773dadbf91fbbfb19221665a14e7df094b5b3
Alec Wells <alec.wells47@gmail.com>
2020-01-13 14:46:20 -0700

    Setup olympians router with index

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -5,3 +5,26 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require('node-fetch');
+
+
+async function allOlympians() {
+  try {
+    let response = await database('olympians').select()
+    return response;
+  } catch(err) {
+    return err;
+  }
+}
+
+router.get('/', (request, response) => {
+  allOlympians()
+    .then(olympians => {
+      if (olympians.length) {
+        response.status(200).send(olympians)
+      } else {
+        response.status(404).json({
+          error: `No olympians found`
+        });
+      }
+    })
+  });
