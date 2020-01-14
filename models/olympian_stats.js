f5d585162e6e9b3cab0f08f7268cf8823e38af41
Alec Wells <alec.wells47@gmail.com>
2020-01-14 15:54:01 -0700

    Finish olympian stat model for formatting stats

--- models/olympian_stats.js
+++ models/olympian_stats.js
@@ -11,14 +11,28 @@ class OlympianStats {
     let statData = {}
     let olympianCount = await database('olympians').select('name', 'team', 'age', 'sport', 'sex', 'height', 'weight')
                               .groupBy('name', 'team', 'age', 'sport', 'sex', 'height', 'weight')
-    await console.log(olympianCount.length, "Olympian count")
     statData["total_competing_olympians"] = await olympianCount.length
 
     let avgAge = await database('olympians').avg('age')
     let formattedAvgAge = await parseFloat(avgAge[0]["avg"])
     let finalAvg = await Math.round(formattedAvgAge * 10) / 10;
     statData["average_age"] = await finalAvg
-    return statData
+    statData["average_weight"] = await {}
+    statData["average_weight"]["unit"] = "kg"
+
+    let maleAvgWeight = await database('olympians').where('sex', 'M').whereNotNull('weight').avg('weight')
+    let femaleAvgWeight = await database('olympians').where('sex', 'F').whereNotNull('weight').avg('weight')
+    let maleFormattedAvgWeight = await parseFloat(maleAvgWeight[0]["avg"])
+    let femaleFormattedAvgWeight = await parseFloat(femaleAvgWeight[0]["avg"])
+    let finalMaleAvg = await Math.round(maleFormattedAvgWeight * 10) / 10;
+    let finalFemaleAvg = await Math.round(femaleFormattedAvgWeight * 10) / 10;
+
+    statData["average_weight"]["male_olympians"] = await finalMaleAvg
+    statData["average_weight"]["female_olympians"] = await finalFemaleAvg
+
+    let finalStatData = {}
+    finalStatData["olympian_stats"] = await statData
+    return finalStatData
   }
 
 }
