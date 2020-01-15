1c7f9cdbf52883d35ac2518d341c3c533d14fa48
Alec Wells <alec.wells47@gmail.com>
2020-01-15 06:48:32 -0700

    Add /api/v1/events/:id/medalists to events router

--- routes/api/v1/events.js
+++ routes/api/v1/events.js
@@ -19,4 +19,17 @@ router.get('/', async function (request, response) {
   })
 })
 
+router.get('/:id/medalists', async function (request, response) {
+  let id = request.params.id
+  let eventsModel = await new Events()
+
+  await eventsModel.getmedals(id)
+  .then((data) => {
+    response.status(200).json(data);
+  })
+  .catch((error) => {
+    return response.status(404).json({ error: "Event not Found" })
+  })
+})
+
   module.exports = router;
